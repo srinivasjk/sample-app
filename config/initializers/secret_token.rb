b45122288b74bb397ddcf943c2bcef17aa518e54
Srinivas Jonnakuty <sjonnakuty@groupon.com>
2013-08-02 11:33:01 -0700

    Add a StaticPages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd30750a5cddf7b8d5dd999c077b4b9fbec993ab76ddfadcccb008bd6fd0f5131471bd0ff77c41e896fad99c53704488b98f3d475104152b8704513f0162ad782'
