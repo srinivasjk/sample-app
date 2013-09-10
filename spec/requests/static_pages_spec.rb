ebf6c32e7b349f21a6f1790d390159c949ac1b84
Srinivas Jonnakuty <srinivas.jk@outlook.com>
2013-09-10 16:54:56 -0700

    my first git commit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,37 +4,44 @@ describe "StaticPages" do
   describe "Home page" do
     it "should have content 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
+      visit '/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have right title " do
-		visit '/static_pages/home'
+		visit '/home'
     	page.should have_selector('title', :text => "Ruby on Rails Sample | Home")
     end
   end
 
   describe "Help Page" do
   	it "should have content 'Help'" do
-  		visit '/static_pages/help'
+  		visit '/help'
   		page.should have_selector('h1', :text => 'Help')
   	end
 
   	it "should have right title" do
-  		visit "/static_pages/help"
+  		visit "/help"
   		page.should have_selector("title", :text => "Ruby on Rails Sample | Help")
   	end
   end
 
   describe "About page" do
   	it "should have content 'About Us'" do
-  		visit '/static_pages/about'
+  		visit '/about'
   		page.should have_selector('h1', :text => 'About Us')
   	end
 
   	it "should have right title" do
-  		visit "/static_pages/about"
+  		visit "/about"
   		page.should have_selector("title", :text => "Ruby on Rails Sample | About")
   	end
+  end
+
+  describe "Contact Page" do
+    it "should have content h1 'Contact' " do
+      visit '/contact'
+      page.should have_selector('h1',  text: 'Contact')
+    end
   end
 end
